4f492772f522834b30be14823fc71b3110999bfd
DiegoCuba <diego.netsolutions@gmail.com>
2021-05-31 21:25:51 -0400

    architecture/basic router implemented and Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import Commits from "./components/Commits";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import NavbarComponent from "./components/Navbar";
+import routes from "./constants/routes";
+import styled from 'styled-components'
+import landingBackground from './assets/imgs/landingBackground.jpg'
 
+const MainApp=styled.div`
+height:100vh;
+background-color:black;// fallback
+background-image:url(${landingBackground});
+background-position: center;
+background-size: cover;
+background-repeat: no-repeat;
+`
 
 function App() {
   return (
-    <div >
-   <Commits />
-    </div>
+    <MainApp>
+      <Router>
+        <NavbarComponent />
+        <Switch>
+          {routes.map((item) => (
+            <Route
+              key={item.path}
+              path={item.path}
+              component={item.component}
+              exact={item.exact}
+            />
+          ))}
+        </Switch>
+
+      </Router>
+    </MainApp>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
+
     <App />
-  </React.StrictMode>
+
   </Provider>,
   document.getElementById('root')
 );
