33cee5aeb59c8f0d3dbb6f4d0045c7d6bce70bf3
DiegoCuba <diego.netsolutions@gmail.com>
2021-06-04 12:03:25 -0400

    actions test done

---
+++ src/redux/repositories/actions.test.js
@@ -0,0 +1,42 @@
+import { errorFetchingRepositories, getRepositories, saveFetchedRepositories } from "./actions";
+import { GET_REPOSITORIES_FAILED, GET_REPOSITORIES_REQUEST, GET_REPOSITORIES_SUCCESS } from "./types";
+import mockRepositories from '../../constants/test-mock-data/mockRepositories'
+
+
+// Tests
+describe('Repositories Actions', () => {
+    it('Gets All Repositories', () => {
+        const payload = { ramdomPayload: 'XYZ' }
+        const action = getRepositories(payload);
+
+        expect(action).toEqual({
+            type: GET_REPOSITORIES_REQUEST,
+            payload
+        });
+    });
+
+    it('Save fetched Repositories', () => {
+
+        const links = ''
+        const repositories = mockRepositories
+        const action = saveFetchedRepositories({ repositories, links });
+
+        expect(action).toEqual({
+            type: GET_REPOSITORIES_SUCCESS,
+            repositories,
+            links
+        });
+    });
+
+    it('Fetching Error', () => {
+
+        const action = errorFetchingRepositories();
+
+        expect(action).toEqual({
+            type: GET_REPOSITORIES_FAILED
+        });
+    });
+
+
+});
+
