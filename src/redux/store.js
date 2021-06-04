e211e4cbcfbb71886852d9eb14f5441b23a05dd9
DiegoCuba <diego.netsolutions@gmail.com>
2021-06-04 00:50:50 -0400

    added redux-devtools-extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,17 @@
-import { createStore, compose, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './rootReducer'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './rootSaga'
+// Chrome Dev Tools
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const sagaMiddleware = createSagaMiddleware()
 
-const store = compose(
-    applyMiddleware(sagaMiddleware),
-
-    window.__REDUX_DEVTOOLS_EXTENSION__
-        ? window.__REDUX_DEVTOOLS_EXTENSION__()
-        : f => f // So test can work properly
-)(createStore)(rootReducer)
+const store = createStore(
+    rootReducer,
+    undefined,
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 
 sagaMiddleware.run(rootSaga)
 
