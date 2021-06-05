856f5794fe818d98cb6c94798ba9f21803b4b40f
DiegoCuba <diego.netsolutions@gmail.com>
2021-06-04 21:34:01 -0400

    NavbarComponent tests done

---
+++ src/components/Navbar.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+
+import NavbarComponent from './Navbar';
+import { customRender } from '../setupTests';
+import routes from '../constants/routes'
+
+describe('NavbarComponent test', () => {
+    const user = 'XXX_USER_XXX'
+    const repo = 'XXX_REPO_XXX'
+
+    it('render about link', () => {
+        customRender(<NavbarComponent />, { initialState: { repo: { user, repo } } })
+
+        screen.getAllByText(user).forEach(item => {
+            expect(item).toBeInTheDocument();
+        })
+        screen.getAllByText(repo).forEach(item => {
+            expect(item).toBeInTheDocument()
+        })
+    })
+
+    it('Render each Link declared in ./constants/routes.js', () => {
+        routes.forEach(item => {
+            expect(screen.getByText(item.main)).toBeInTheDocument()
+        })
+    })
+
+})
--- src/components/Pagination.test.js
+++ src/components/Pagination.test.js
@@ -1,5 +1,5 @@
 
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { mockLinks } from '../constants/test-mock-data/mockLinks'
 import Pagination from './Pagination';
 
